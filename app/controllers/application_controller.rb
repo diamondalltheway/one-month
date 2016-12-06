056fcb632bc84c9b19c1fc339f54ab5cb5fe8541
Hunter Stevens <hunter.stevens@hotmail.com>
2016-12-06 21:36:01 +0000

    edited application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-    def access_denied(exception)
-     redirect_to root_path, alert: exeption.message
-    end
+    
 end
