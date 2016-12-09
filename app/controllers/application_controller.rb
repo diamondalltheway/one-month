6d76d2ca5e3380ff4f9f2d66d3e25dace5b9d51e
Hunter Stevens <hunter.stevens@hotmail.com>
2016-12-09 15:51:01 +0000

    fixed admin dash access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
     def access_denied(exception)
-      redirect_to root_path, alert: exeption.message
+      redirect_to root_path, alert: exception.message
     end
-   
 end
