93c4c98b76a3045e79920ac7eefa4e3b00eaa77b
Hunter Stevens <hunter.stevens@hotmail.com>
2016-12-12 20:44:25 +0000

    undo changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+   ActiveAdmin.routes(self)
   devise_for :users
-  as :user do
-  	get 'signin' => 'devise/sessions#new'
-  	delete 'signout' => 'devise/sessions#destroy'
-  	get 'signup' => 'devise/registrations#new'
+  devise_scope :user do
+    get 'signin' => 'devise/sessions#new'
+    delete 'signout' => 'devise/sessions#destroy'
+    get 'signup' => 'devise/registrations#new'
   end
+  
+  
+  
+   
+   
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contact-us' => 'pages#contact'
-
+  get 'contact' => 'pages#contact-us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
